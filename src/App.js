c5b9fd2f0b9e59882ab807c634e62311f1bb1034
Abdul quddos <abdulquddoshafiz@gmail.com>
2023-04-19 16:44:14 +0500

    initial things

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { Typography,AppBar,Card,CardActions,CardMedia,CssBaseline,Grid,Toolbar,Container } from '@mui/material';
+import { PhotoCamera } from '@mui/icons-material';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <CssBaseline/>
+   <AppBar position='relative'>
+   <Toolbar>
+    <PhotoCamera/>
+    <Typography variant='h6'>
+     Photo Album
+    </Typography>
+   </Toolbar>
+   </AppBar>
+   <main>
+    <div>
+      <Container maxWidth='sm'>
+        <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+          Photo Album
+        </Typography>
+        <Typography variabt='h5' align='center' color='textSecondary' paragraph>
+          This is photo album, how does it look like on the screen
+          </Typography>
+      </Container>
     </div>
+   </main>
+   </>
   );
 }
-
 export default App;
